7410d6186bc38d1cf0eb1606997cfe4a398ca798
ispiceup <atoijuliet@gmail.com>
2022-10-16 16:41:50 -0700

    talk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome 
-is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
