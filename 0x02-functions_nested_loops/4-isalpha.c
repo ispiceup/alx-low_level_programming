b3f56aed36b326295540ca10af4c99d765c3198f
ispiceup <atoijuliet@gmail.com>
2022-10-18 23:37:30 -0700

    do

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - entry point
+ * Description: Returns 1 if c is a letter, lowercase or uppercase
+ *
+ * Return: 0 or 1
+ */
+
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
