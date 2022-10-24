b36be237f4d8b337e31645d8bf5524777efa3f53
ispiceup <atoijuliet@gmail.com>
2022-10-24 15:39:30 -0700

    likr

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts2  -> function that prints one character out of two
+* @str: input
+* Return: print
+*/
+
+void puts2(char *str);
+{
+	int a = 0;
+
+	for (; str[a] != '\0'; a++
+
+	{
+		if ((a % 2) == o)
+			_putchar(str[a]);
+		else
+			continue;
+	}
+	_putchar('\n')
+}
