100b6618d2b4fa9615e16725f3e918072713e887
ispiceup <atoijuliet@gmail.com>
2022-10-24 15:16:00 -0700

    men

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,18 +3,18 @@
 /**
 * rev_string -> function that prints a string, in reverse,
 * @s: input
-* Return: reversed stringI
+* Return: reversed string
 */
 
 void rev_string(char *s)
 {
 	char rv = s[0];
-	int a = 0
+	int a = 0;
 	int b;
 
 	while (s[a] != '\0')
-                  a++;
-       for (b = 0; b < a; b++)
+		a++;
+	for (b = 0; b < a; b++)
 	{
 		a--;
 		rv = s[b];
@@ -23,6 +23,4 @@ void rev_string(char *s)
 
 	}
 
-
 }
-~                 
