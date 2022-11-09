41cba12837fef53deea1dce93b65a20fac020722
ispiceup <atoijuliet@gmail.com>
2022-11-08 16:12:29 -0800

    hmm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+/**
+*main - Entry point
+*
+*Description: prints all the arguments separated by a new line
+*atoi is a function that converts a string into an int
+*@argc: number of arguments.
+*@argv: pointer containing the arrays of the arguments
+*
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+         
