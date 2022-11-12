57bfea657bcfe60ffa75caa215901ce88822a3d6
ispiceup <atoijuliet@gmail.com>
2022-11-12 09:21:12 -0800

    learn

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints a string in reverse and includes a new line
+ *@s: pointer that has the address of the string to be printed
+ *Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+
+		_print_rev_recursion(s++);
+	else
+		return;
+	_putchar(*s);
+}
