17e8fde24a0dec55f270243be849f77400c1c08b
ispiceup <atoijuliet@gmail.com>
2022-11-14 13:30:01 -0800

    know

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * init_dog 
+  * @d: 
+  * @name: dog name
+  * @age: age of dog
+  * @owner: name of dog owner
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
