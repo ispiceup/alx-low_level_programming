0dcee90e937209c10604c72a96937c63c2915705
ispiceup <atoijuliet@gmail.com>
2022-11-16 13:34:04 -0800

    nuh

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+* print_name - prints a name
+* @name: string
+* @f: function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
