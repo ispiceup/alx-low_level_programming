0f924882a983969969741ec81b7cf88ec98d8ad0
ispiceup <atoijuliet@gmail.com>
2022-11-17 12:33:47 -0800

    hard

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ * @a: value to be added
+ * @b: scound value to be added
+ * Return: sum of a+b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: 1st value.
+ * @b: 2nd value.
+ * Return: diferrence of a -b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of a*b
+ * @a: number
+ * @b: number
+ * Return: result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a/b
+ * @a: first figure
+ * @b: second figure
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - divides and gives the remainder
+ * @a: first figure
+ * @b: second figure
+ * Return: result of modulation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
