d1fbad1eabfc14cf1f7f7dfac0f8c27ea2fec016
ispiceup <atoijuliet@gmail.com>
2022-11-26 12:52:07 -0800

    do

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+
 /**
- * main -
- * main - prints numbers 0-9 ,
- *  2x max
-(*
- * Return: 0 on success
- */
+* main -
+* main - prints numbers 0-9 ,
+*  2x max
+*
+* Return: 0 on success
+*/
 int main(void)
 {
 
-	int i = '0';
+	int i = 0;
 
-	while (i <= '9')
+	while (i < 9)
 	{
 		putchar(i);
 		i++;
 	}
 	putchar('\n');
+	
 	return (0);
+}
