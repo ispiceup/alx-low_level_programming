dcedd248fc6d692ca8599141d8c6aca6309a9a90
ispiceup <atoijuliet@gmail.com>
2022-12-19 19:03:47 -0800

    hard

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * reverse_listint - function that reverses a listint_t linked list.
+  * @head: 
+  * Return: a pointer to the first node of the reversed list
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
